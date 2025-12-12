cc046a7a412011369b54dd7005ced2c213eb7fab
Joshua Winters-Brown <joshuawintersbrown@gmail.com>
2025-12-12 09:18:40 -0500

    regress auth

--- backend/internal/db/models/users/user.go
+++ backend/internal/db/models/users/user.go
@@ -4,6 +4,9 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model        // GORM provides ID, CreatedAt, UpdatedAt, DeletedAt
+	Email      string `gorm:"uniqueIndex;not null"`
+	FirstName  string `gorm:"not null"`
+	LastName   string `gorm:"not null"`
 	Username   string `gorm:"uniqueIndex;not null"`
 	APIKey     string `gorm:"uniqueIndex;not null"`
 }
